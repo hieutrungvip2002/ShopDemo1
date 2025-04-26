41be0c6c4ab0ab79069b9b53bf91e7085fae11dd
hieucode2x <hieutrungvip2002@gmail.com>
2025-04-26 10:40:53 +0700

    finish code view gio hang

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT24CNTT02/mvcdemo/',
+    'baseURL' => 'http://localhost:88/PTIT24CNTT02/mvcdemo/',
+    'assets' => '/PTIT24CNTT02/mvcdemo/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
