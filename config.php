c72e313a607f0d3d5139233a44f439f34b5bef04
hieucode2x <hieutrungvip2002@gmail.com>
2025-04-28 15:31:06 +0700

    da hoan thanh gui email va form lien he

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT2402/ShopDemo1/',
+    'baseURL' => 'http://localhost/PTIT2402/ShopDemo1/',
+    'assets' => '/PTIT2402/ShopDemo1/assets',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'productdb',
+        'username' => 'root',
+        'password' => ''
+    )
+);
